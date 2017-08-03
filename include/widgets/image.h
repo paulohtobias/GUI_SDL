f83e8db0ba54888b6a5f299196ee90e07ce86eb0
Paulo Tobias <paulohtobias@outlook.com>
2017-08-03 15:18:50 -0300

    Image Widget Created

---
+++ include/widgets/image.h
@@ -0,0 +1,36 @@
+/*
+ * Paulo Henrique Tobias
+ * Manga Library
+ *
+ * Image
+ * Image library using SDL2
+ */
+
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include "widgets/texture_widget.h"
+#include <SDL2/SDL_image.h>
+
+typedef struct Image{
+    TextureWidget t_widget;
+    
+    char *file;
+}Image;
+
+Image new_Image(char *file);
+
+Image new_Image_with_position(char *file, Position position);
+
+Image new_Image_with_bounds(char *file, SDL_Rect bounds);
+
+void generic_image_init(void *raw_image, SDL_Renderer *renderer);
+
+void generic_image_set_changed(void *raw_image, bool changed);
+
+void generic_image_update(void *raw_image, SDL_Renderer *renderer);
+
+void image_set_file(Image *image, char *file);
+
+#endif //IMAGE_H
+
---
+++ src/widgets/image.c
@@ -0,0 +1,108 @@
+/*
+ * Paulo Henrique Tobias
+ * Manga Library
+ *
+ * Image
+ * Image library using SDL2
+ */
+
+#include "widgets/image.h"
+
+Image new_Image(char *file){
+    return new_Image_with_bounds(file, new_rect(0, 0, 0, 0));
+}
+
+Image new_Image_with_position(char *file, Position position){
+    return new_Image_with_bounds(file, new_rect(position.x, position.y, 0, 0));
+}
+
+Image new_Image_with_bounds(char *file, SDL_Rect bounds){
+    Image image;
+    
+    image.t_widget = new_TextureWidget();
+    
+    image_set_file(&image, file);
+    set_bounds_from_SDL_Rect((void *)&image, bounds);
+    
+    image.t_widget.widget.init = generic_image_init;
+    image.t_widget.set_changed = generic_image_set_changed;
+    image.t_widget.update = generic_image_update;
+    
+    return image;
+}
+
+void generic_image_init(void *raw_image, SDL_Renderer *renderer){
+    Image *image = raw_image;
+    
+    Size size = image->t_widget.widget.bounds.size;
+    if(size.w > 0 && size.h > 0){
+        return;
+    }
+    SDL_Surface *temp = IMG_Load(image->file);
+    if(temp == NULL){
+        printf("init_image: Error Opening Image <%s>: %s\n", image->file, IMG_GetError());
+        image_set_file(image, "./Resources/Images/default_image.png");
+        temp = IMG_Load(image->file);
+    }
+    size.w = temp->w;
+    size.h = temp->h;
+
+    SDL_FreeSurface(temp);
+    temp = NULL;
+
+    set_size(&image->t_widget.widget.bounds, size);
+    image->t_widget.set_changed(raw_image, true);
+}
+
+void generic_image_set_changed(void *raw_image, bool changed){
+    ((Image *)raw_image)->t_widget.widget.state.changed = changed;
+}
+
+void generic_image_update(void *raw_image, SDL_Renderer *renderer){
+    Image *image = raw_image;
+    
+    if(image->t_widget.widget.state.changed == false){
+        return;
+    }
+    
+    //load the image into memory using SDL_image library function
+    int error_count = 0;
+    SDL_Surface *surface = IMG_Load(image->file);
+    if(!surface){
+        img_error:
+        //SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error opening image", IMG_GetError(), NULL);
+        printf("image_update: Error Opening Image <%s>: %s\n", image->file, IMG_GetError());
+        image_set_file(image, "./Resources/Images/default_image.png");
+        //img->surface = IMG_Load(img->file);
+        surface = IMG_Load(image->file);
+    }
+
+    // load the image data into the graphics hardware's memory
+    if(image->t_widget.texture != NULL){
+        SDL_DestroyTexture(image->t_widget.texture);
+        image->t_widget.texture = NULL;
+    }
+    image->t_widget.texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    surface = NULL;
+    if(image->t_widget.texture == NULL){
+        if(error_count == 0){
+            error_count++;
+            goto img_error;
+        }
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error creating image texture", SDL_GetError(), NULL);
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        exit(1);
+    }
+    //image_setBounds(img, image_getBounds(img));
+    image->t_widget.set_changed(raw_image, false);
+}
+
+void image_set_file(Image *image, char *file){
+    if(image->file != NULL){
+        free(image->file);
+    }
+    image->file = malloc(sizeof(file));
+    strcpy(image->file, file);
+}
